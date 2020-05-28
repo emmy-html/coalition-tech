fe9a18fee9f09c00743756d5b4383c8a6a2a8772
Emmy <60683173+emmy-html@users.noreply.github.com>
2020-05-28 12:48:43 -0500

    Updated main.js so the background image changes according to which schedule is being viewed

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
-function openTab(tabName, elmnt) {
+function openTab(tabName) {
     // hide all tabs by default
-    var i, tabcontent, tablinks;
+    var i, tabcontent, tablinks, img;
     tabcontent = document.getElementsByClassName("tab-content");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
     // show the specified tab content
     document.getElementById(tabName).style.display = "inherit";
+    // add background image on click
+    if (tabName === 'schedule-one') {
+        var image = document.getElementById("schedule-background");
+            image.style.backgroundImage = 'url(' + './img/tab1.png' + ')';
+    } else if (tabName === 'schedule-two'){
+        var image = document.getElementById("schedule-background");
+        image.style.backgroundImage = 'url(' + './img/tab2.png' + ')';
+    }
 }
-
 // get the element with id="defaultOpen" and click on it
 
 document.getElementById("defaultOpen").click();
